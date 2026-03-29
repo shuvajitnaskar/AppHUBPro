37f8716d61db5dd57bce2a139dbba131057ee565
Shuvajit Naskar <shuvajitnaskar374@gmail.com>
2026-03-29 15:43:53 +0530

    Updated allowed hosts

--- apphubpro/settings.py
+++ apphubpro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zm86l8pg#xs7j&76shkei7r3xkx#+&r6yn#zbk3xfkm@=jr(b=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
